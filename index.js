709d1eedf9016044897480d3c23303a16303fc53
NooranTarek <noort.mohamed@gmail.com>
2024-07-09 19:28:50 +0300

    adding catch async error handling and global error handling

--- index.js
+++ index.js
@@ -7,5 +7,24 @@ const port = 3000
 app.use(express.json());
 
 dbConnection();
+
+app.use((error, req, res, next) => {
+    let statusCode = error.statusCode || 500;
+    let message = error.message || 'Internal Server Error';
+  
+    if (error.name === 'ValidationError') {
+      statusCode = 400;
+      message = Object.values(error.errors).map(err => err.message).join(', ');
+    }
+  
+    if (error.code && error.code === 11000) {
+      statusCode = 400;
+      const field = Object.keys(error.keyValue);
+      message = `${field} already exists.`;
+    }
+  
+     return res.status(statusCode).json(message);
+  });
+  
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/utilities/errorHandling.js
@@ -0,0 +1,9 @@
+
+export const catchAsyncErr=(fn)=>{
+    return (req,res,next)=>{
+        fn(req,res,next).catch((error)=>{
+            next(error)
+        })
+    }
+  }
+
